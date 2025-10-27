503c37e5ac942b900c77fb43b42dcbfd837d2a0f
IsaiahWu <chunyueh.wu@student.uq.edu.au>
2025-10-28 01:30:25 +1000

    Chnage pair_image_labels

--- siamese-4986525/dataset.py
+++ siamese-4986525/dataset.py
@@ -4,16 +4,16 @@ from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 import os
 from PIL import Image
+import config
 
 
-class Siamese_Datase(Dataset):
-  def __init__(self, store_path, train_image, train_labels, transform = None):
-    self.train_image = train_image
-    self.labels_df = pd.read_csv(train_labels)
-    self.transform = transform
-    self.store_path = store_path
-    self.create_labels()
-    self.pairs = self.pair_image_label()
+class SiameseDataset(Dataset):
+  def __init__(self, image_dir, labels_csv, transform=None):
+      self.image_dir = config.train_image
+      self.labels_df = pd.read_csv(config.train_labels)
+      self.transform = transform
+      self.create_labels()
+      self.pairs = self.pairs_image_label()
 
   def create_labels(self):
     labels = [] #normal or melonma
@@ -25,23 +25,28 @@ class Siamese_Datase(Dataset):
     self.labels_df['binary_label'] = labels  # Make it to binary labels
 
 
-  def pair_image_label (self):
-    image_names = os.listdir(self.train_image)
+# pairs images with labels
+  def pairs_image_label(self):
+    image_names = os.listdir(self.image_dir)
+    image_dict = {os.path.splitext(name)[0]: name for name in image_names}
     pairs = []
-    for image_name in image_names:
-      match_name = self.labels_df[self.labels_df["image_name"] == image_name]
-      if not match_name.empty:
-        label = match_name.iloc[0]["binary_label"] # get the label, binary_label
-        full_path = os.path.join(self.train_image, image_name) #access to each indivual iamge
-        pairs.append((full_path, label))  #("a.jpg", 1)
+    for _, row in self.labels_df.iterrows():
+        image_id = str(row["image_name"]).strip()  # Defensive strip
+        label = row["binary_label"]
+        if image_id in image_dict:
+            full_path = os.path.join(self.image_dir, image_dict[image_id])
+            pairs.append((full_path, label))
+    print(f"Loaded {len(pairs)} image pairs from {self.image_dir}")
     return pairs
-  
-  
-  
+
+
+
   # How many pairs of images
   def __len__(self):
     return len(self.pairs)
 
+
+# Preparing labels to comapre
   def __getitem__(self, index):
     img_1, label_1 = self.pairs[index] #sperating image and pair
     same_class = random.randint(0, 1) # Decide if second image should be same class or not
@@ -49,13 +54,13 @@ class Siamese_Datase(Dataset):
       while True:
         img_2, label_2 = random.choice(self.pairs) #pick second image randonmly
         if label_1 == label_2:
-          pair_label = 1
+          pairs_label = 1
           break
     else:
       while True:
         img_2, label_2 = random.choice(self.pairs)
         if label_1 != label_2:
-          pair_label = 0
+          pairs_label = 0
           break
     img_1 = Image.open(img_1).convert("RGB")
     img_2 = Image.open(img_2).convert("RGB")
@@ -65,13 +70,4 @@ class Siamese_Datase(Dataset):
       img_1 = self.transform(img_1)
       img_2 = self.transform(img_2)
 
-    return img_1, img_2, pair_label
-
-
-
-
-
-
-
-
-
+    return img_1, img_2, pairs_label
